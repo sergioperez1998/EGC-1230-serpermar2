db2c450614fda183326c48ccb0cbc5c03546db3b
sergioperez1998 <sergio-pm@hotmail.com>
2020-09-07 12:37:04 +0200

    probando

--- decide/travis_local_settings.py
+++ decide/travis_local_settings.py
@@ -14,25 +14,24 @@ MODULES = [
 ]
 
 APIS = {
-    'authentication': 'http://127.0.0.1:8000',
-    'base': 'http://127.0.0.1:8000',
-    'booth': 'http://127.0.0.1:8000',
-    'census': 'http://127.0.0.1:8000',
-    'mixnet': 'http://127.0.0.1:8000',
-    'postproc': 'http://127.0.0.1:8000',
-    'store': 'http://127.0.0.1:8000',
-    'visualizer': 'http://127.0.0.1:8000',
-    'voting': 'http://127.0.0.1:8000',
+    'authentication': 'http://localhost:8000',
+    'base': 'http://localhost:8000',
+    'booth': 'http://localhost:8000',
+    'census': 'http://localhost:8000',
+    'mixnet': 'http://localhost:8000',
+    'postproc': 'http://localhost:8000',
+    'store': 'http://localhost:8000',
+    'visualizer': 'http://localhost:8000',
+    'voting': 'http://localhost:8000',
 }
 
-BASEURL = 'http://127.0.0.1:8000'
+BASEURL = 'http://localhost:8000'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_decide',
-        'USER': 'decide',
-        'PASSWORD': 'decide',
+        'NAME': 'postgres',
+        'USER': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
