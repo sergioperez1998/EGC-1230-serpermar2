78ee43442be9851c9bc1b310e7d7a005ad55c6cd
sergioperez1998 <sergio-pm@hotmail.com>
2020-09-07 12:12:53 +0200

    Despliegue heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -68,7 +69,19 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://examen-egc.herokuapp.com'
+
+APIS = {
+    	'authentication': 'https://examen-egc.herokuapp.com', 
+    	'base': 'https://examen-egc.herokuapp.com',
+    	'booth': 'https://examen-egc.herokuapp.com',
+    	'census': 'https://examen-egc.herokuapp.com',
+    	'mixnet': 'https://examen-egc.herokuapp.com',
+   		'postproc': 'https://examen-egc.herokuapp.com',
+    	'store': 'https://examen-egc.herokuapp.com',
+    	'visualizer': 'https://examen-egc.herokuapp.com',
+    	'voting': 'https://examen-egc.herokuapp.com',
+		}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -156,7 +169,9 @@ KEYBITS = 256
 try:
     from local_settings import *
 except ImportError:
-    print("local_settings.py not found")
+    #print("local_settings.py not found")
+    pass
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+django_heroku.settings(locals())
